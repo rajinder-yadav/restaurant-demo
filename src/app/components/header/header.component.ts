a001fbc1a70f73d19ebdd2db98b7e9d22388c045
Rajinder Yadav <devguy.ca@gmail.com>
2019-02-12 16:49:54 -0500

    Configurable with styling complete.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ry-header',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() text: string;
 
   constructor() { }
 
