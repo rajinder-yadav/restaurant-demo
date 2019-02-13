acab5f47163a58e75bb1f459f2626a2483fc18e3
Rajinder Yadav <devguy.ca@gmail.com>
2019-02-12 20:49:10 -0500

    Layout complete.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Topic } from '../../data/data.topics';
+import { TOPICS } from '../../data/topic.mock.data';
+
 @Component({
   selector: 'ry-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  readonly topics: Topic[] = TOPICS;
+
+  title = 'Ristorante Il Fornello';
+
+  text = 'Simple, fresh ingredients, prepared and cooked with love and \
+    passion become truly great dishes, from wood-oven baked Neapolitan \
+    style pizzas to pastas, entrees!';
 
   constructor() { }
 
