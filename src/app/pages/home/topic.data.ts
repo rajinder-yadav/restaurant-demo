66d5f30bb0bf21353b700770078dc94072982170
Rajinder Yadav <devguy.ca@gmail.com>
2019-02-12 19:41:58 -0500

    Mock data.

---
+++ src/app/pages/home/topic.data.ts
@@ -0,0 +1,24 @@
+export const TOPICS = [
+  {
+    title: 'Fichipizza',
+    text: 'A unique combination of Mediterranean and Italian pizza, \
+      topped with Mascarphone, proscuitto, figs, grando panado, honey and arugula.',
+    caption: 'Our Unique Culinary Creations'
+  },
+  {
+    title: 'Weekend Grand Buffet',
+    text: 'Featuring mouthwatering combinations with a choice of five \
+      different salads, six enticing appetizers, six main entrees and \
+      five choicest desserts. Free flowing bubbly and soft drinks. \
+      All for just $19.99 per person',
+    caption: 'This Month\'s Promotions'
+  },
+  {
+    title: 'Tommy McHugh',
+    subtitle: 'Executive Chef',
+    text: 'Award winning three-star Michelin chef with wide International \
+      experience having worked closely with whos-who in the culinary world, \
+      he specializes in creating mouthwatering Mediterranean-Italian experiences.',
+    caption: 'Meet our Culinary Specialists'
+  }
+];
